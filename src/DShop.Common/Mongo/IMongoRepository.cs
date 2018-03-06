3fcb1c33b597301e4be5114992dbf4ea29a2973e
GooRiOn <foreverframenet@gmail.com>
2018-03-06 19:32:48 +0100

    Added FindAsync in mongo repository

--- src/DShop.Common/Mongo/IMongoRepository.cs
+++ src/DShop.Common/Mongo/IMongoRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using DShop.Common.Types;
@@ -10,6 +11,7 @@ namespace DShop.Common.Mongo
     {
          Task<TEntity> GetAsync(Guid id);
          Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+         Task<IEnumerable<TEntity>> FindAsync<TQuery>(Expression<Func<TEntity, bool>> predicate);
          Task<PagedResult<TEntity>> BrowseAsync<TQuery>(Expression<Func<TEntity, bool>> predicate,
 				TQuery query) where TQuery : PagedQueryBase;
          Task CreateAsync(TEntity entity);
