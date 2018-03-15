29aa5cdbe65638a68b50d79f1aef8e4eb1c6c957
GooRiOn <foreverframenet@gmail.com>
2018-03-15 19:45:16 +0100

    Added Emial property to MailKitOptions

--- src/DShop.Common/MailKit/MailKitOptions.cs
+++ src/DShop.Common/MailKit/MailKitOptions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DShop.Common.MailKit
+﻿namespace DShop.Common.MailKit
 {
     public class MailKitOptions
     {
@@ -10,5 +6,6 @@ namespace DShop.Common.MailKit
         public int Port { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
