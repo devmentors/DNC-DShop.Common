11d3e657fc60e10f2bec5ce4d3687393ac06e68c
GooRiOn <foreverframenet@gmail.com>
2018-03-15 18:43:01 +0100

    Added MailKit section.

---
+++ src/DShop.Common/MailKit/MailKitOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DShop.Common.MailKit
+{
+    public class MailKitOptions
+    {
+        public string SmtpHost { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
