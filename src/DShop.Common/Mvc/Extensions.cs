1ac06ec1b408fbacd4930f8c03c0a680c43a93c3
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-15 13:28:16 +0100

    Lockbox integration

--- src/DShop.Common/Mvc/Extensions.cs
+++ src/DShop.Common/Mvc/Extensions.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using Lockbox.Client.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using System;
 
 namespace DShop.Common.Mvc
 {
@@ -22,5 +26,15 @@ namespace DShop.Common.Mvc
 
         public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder builder)
             => builder.UseMiddleware<ErrorHandlerMiddleware>();
+
+        public static IWebHostBuilder UseLockbox(this IWebHostBuilder builder)
+            => builder.ConfigureAppConfiguration((ctx, cfg) => 
+                {
+                    var useLockbox = Environment.GetEnvironmentVariable("USE_LOCKBOX");
+                    if (useLockbox?.ToLowerInvariant() == "true")
+                    {
+                        cfg.AddLockbox();
+                    }
+                });
     }
 }
