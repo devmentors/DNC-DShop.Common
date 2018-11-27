0c1354b1ef3c0d081a753d1cb9f8e2fc20de1617
Piotr Gankiewicz <spetz90@gmail.com>
2018-11-27 19:17:55 +0100

    Updated packages & scripts

--- scripts/dotnet-build.sh
+++ scripts/dotnet-build.sh
@@ -6,4 +6,4 @@ case "$TRAVIS_BRANCH" in
     ;;
 esac
 
-dotnet build -c Release --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json" --no-cache
+dotnet build -c Release --no-cache
